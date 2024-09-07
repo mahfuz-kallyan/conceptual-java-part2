fd1a0644696e0124af7057ff566e49f7dcb7dab6
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-09-07 13:28:03 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+const x = [32,4,65,67,76];
+// console.log(x.length);
+// x[3]=30;
+x.push(200, 300);
+// console.log(x);
+const y = x[1]
+// console.log(y);
+
+const p = x.pop();
+console.log(x);
+
+console.log(p);
+
