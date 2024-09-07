e06083b2481c63db41efb83dfed404c04119b580
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-09-07 14:43:10 +0600

    added array

--- array.js
+++ array.js
@@ -7,7 +7,27 @@ const y = x[1]
 // console.log(y);
 
 const p = x.pop();
-console.log(x);
+// console.log(x);
 
-console.log(p);
+// console.log(p);
+
+const fruits = ['mango', 'banana', 'apple', 'guava'];
+const flower = ['lily', 'rose', 'tulip', 'sunflower'];
+const both = fruits.concat(flower);
+console.log(both);
+// console.log(both[4]);
+// console.log(both.indexOf('banana'));
+// console.log(both.includes('apple'));
+
+const add = fruits.join('|')
+// console.log(add);
+
+const add2 = flower.join('|')
+console.log(add2);
+
+
+
+
+
+
 
